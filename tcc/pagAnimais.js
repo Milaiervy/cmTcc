e111d524e888bbbdaf961360ba887644995523ad
Milaiervy <134285654+Milaiervy@users.noreply.github.com>
2023-09-21 08:23:45 -0300

    Add files via upload

---
+++ tcc/pagAnimais.js
@@ -0,0 +1,35 @@
+function redirecionarPinhao() {
+  window.location.href = "pinhao.html";
+}
+
+function redirecionarFred() {
+  window.location.href = "fred.html";
+}
+
+function redirecionarCacau() {
+  window.location.href = "cacau.html";
+}
+
+function redirecionarMel() {
+  window.location.href = "mel.html";
+}
+
+function redirecionarMaggie() {
+  window.location.href = "maggie.html";
+}
+
+function redirecionarRabito() {
+  window.location.href = "rabito.html";
+}
+
+function redirecionarTitico() {
+  window.location.href = "titico.html";
+}
+
+function redirecionarBelinha() {
+  window.location.href = "belinha.html";
+}
+
+function redirecionarAurelio() {
+  window.location.href = "aurelio.html";
+}
